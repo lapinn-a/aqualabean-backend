56ce07f5dcf2fb82a50d4e11e8e3251b7c8df372
Nick Martynov <kolyapusya@icloud.com>
2022-04-05 17:00:35 +0300

    create products

--- nodejs-express-sequelize-postgresql/app/models/product.model.js
+++ nodejs-express-sequelize-postgresql/app/models/product.model.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING
         },
         price: {
-            type: Sequelize.INTEGER
+            type: Sequelize.DECIMAL(10, 2)
         },
         amount: {
             type: Sequelize.INTEGER
