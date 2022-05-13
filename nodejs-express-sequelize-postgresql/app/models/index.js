dfac4c23e88b41bf80ac633fac15bf1c0e55faa0
lapinn_a <lapinn-a@ya.ru>
2022-05-14 01:33:58 +0300

    disable Sequelize logging

--- nodejs-express-sequelize-postgresql/app/models/index.js
+++ nodejs-express-sequelize-postgresql/app/models/index.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   dialect: dbConfig.dialect,
-
+  logging: false,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
